6793579b86720fbfa42a2ba34f31636ae8e09e6a
Rahul Kumar <125582644+RahulKumar3444@users.noreply.github.com>
2024-08-06 08:56:33 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)// we are using nodejs,not browser//
+
+console.log("Rahul")
+
+let name = "Rahul"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+
+//number => 2 to power 53
+// bigint
+//string => ""
+//boolean =>true/false
+// null=>standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof null);
