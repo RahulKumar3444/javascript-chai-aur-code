0767951b99ec35e5a0155cad63264d01d9aaa8c5
Rahul Kumar <125582644+RahulKumar3444@users.noreply.github.com>
2024-08-07 08:52:08 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "rahul"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" =>NaN
+
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0=>false
+//"" => faslse
+//"hitesh" => true
+
+let someNumber = 13
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
